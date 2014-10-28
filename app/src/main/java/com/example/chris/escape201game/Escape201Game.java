6952517ae0ab9e7f61cdcb192c6b7fd57518d57d
ChristopherSanders <christopher.sanders@ttu.edu>
2014-10-28 01:41:27 -0500

    Commit New images and sounds

--- app/src/main/java/com/example/chris/escape201game/Escape201Game.java
+++ app/src/main/java/com/example/chris/escape201game/Escape201Game.java
@@ -1,18 +1,95 @@
 package com.example.chris.escape201game;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class Escape201Game extends ActionBarActivity {
+public class Escape201Game extends Activity {
+
+    private MediaPlayer mPlayer;
+    private int currentSong;
+    Button settingsBtn, levelsBtn, playBtn, level1Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escape201_game);
+
+        settingsBtn = (Button) findViewById(R.id.settingsBtnID);
+        levelsBtn = (Button) findViewById(R.id.levelsBtnID);
+        playBtn = (Button) findViewById(R.id.playBtnID);
+        level1Btn = (Button) findViewById(R.id.level1BtnID);
+
+//        if (getCallingActivity() == LevelandSettingsScreen) {
+//            mPlayer = MediaPlayer.create(this, R.raw.song1);
+//            currentSong = R.raw.song1;
+//        } else if (number == 2) {
+//            mPlayer = MediaPlayer.create(this, R.raw.song2);
+//            currentSong = R.raw.song2;
+//
+//        } else if (number == 3) {
+//            mPlayer = MediaPlayer.create(this, R.raw.song3);
+//            currentSong = R.raw.song3;
+//
+//        } else if (number == 4) {
+//            mPlayer = MediaPlayer.create(this, R.raw.song4);
+//            currentSong = R.raw.song4;
+//
+//            // Defaults are always a good idea:
+//        } else {
+            mPlayer = MediaPlayer.create(this, R.raw.song1);
+            currentSong = R.raw.song1;
+
+//        }
+
+        mPlayer.start();
+
+//        createListeners();
+
+
     }
+
+//    private void startSettingsScreenActivity(int buttonNum) {
+//        Intent intent = new Intent(this, SettingsScreen.class);
+//        intent.putExtra("BUTTON NUMBER", buttonNum);
+//        startActivity(intent);
+//    }
+
+//    private void createListeners(){
+//        settingsBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                startSettingsScreenActivity(1);
+//                // TODO Auto-generated method stub
+//            }
+//        });
+//
+//        levelsBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//            }
+//        });
+//
+//        playBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//            }
+//        });
+//
+//        level1Btn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//            }
+//        });
+//    }
+
+
+
 
 
     @Override
@@ -32,5 +109,10 @@ public class Escape201Game extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void gotoActivity(View v){
+        Intent intent = new Intent(this, LevelandSettingsScreen.class);
+        startActivity(intent);
     }
 }
