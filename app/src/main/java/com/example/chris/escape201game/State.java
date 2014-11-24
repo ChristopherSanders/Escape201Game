58b4ab3ff4ba380546fa3dd22459c4cf9ae00064
sftwr <notnotvictor@gmail.com>
2014-11-24 02:23:11 -0600

    State extends application
    comment inside State:
    
    "by extending Application I can store objects/variables in it which can then be accessed by activities..."

---
+++ app/src/main/java/com/example/chris/escape201game/State.java
@@ -0,0 +1,47 @@
+package com.example.chris.escape201game;
+import android.app.Application;
+
+/**
+ * Victor
+ *
+ * by extending Application I can store objects/variables in it
+ * which can then be accessed by activities using State mystate = ((State) getApplicationContext());
+ * myState.getMusicOn() will call getMusicOn in this class
+ *
+ * store persistent variables that can be get/set by all activities
+ * used for configuring music, sfx, and tips
+ */
+public class State extends Application {
+    private boolean musicOn, sfxOn, tipsOn;
+
+    public State(){
+        musicOn = true;
+        sfxOn = true;
+        tipsOn = false;
+    }
+
+    public boolean getMusicOn(){
+        return musicOn;
+    }
+
+    public void setMusicOn(boolean music){
+        musicOn = music;
+    }
+
+    public boolean getSfxOn(){
+        return sfxOn;
+    }
+
+    public void setSfxOn(boolean sfx){
+        sfxOn = sfx;
+    }
+
+    public boolean getTipsOn(){
+        return tipsOn;
+    }
+
+    public void setTipsOn(boolean tips){
+        tipsOn = tips;
+    }
+
+}
